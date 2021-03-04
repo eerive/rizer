e66948d2ca844d7b5d90ed7eb15eb0adb622bd83
Minh Duc Lam <s.lamminhduc@gmail.com>
2021-03-04 01:28:28 +0100

    + updated to flutter 2.0 + added some test code

--- rizer_app/lib/constants.dart
+++ rizer_app/lib/constants.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-class RizerConstants{
+class RizerConstants {
   static final String _appTitle = "Rizer";
+  static final String _searchTitle = "Search";
+  // Colors
+  static final Color _mainTheme = Colors.amberAccent;
 
   static String get appTitle => _appTitle;
-
-  // Colors
- static final Color _mainTheme = Colors.amberAccent;
+  static String get searchTitle => _searchTitle;
+  static Color get mainTheme => _mainTheme;
 }
--- rizer_app/lib/pages/RizerMainPage.dart
+++ rizer_app/lib/pages/RizerMainPage.dart
@@ -27,7 +27,6 @@ class _RizerMainPageState extends State<RizerMainPage> {
           children: <Widget>[],
         ),
         color: Colors.white,
-
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.white,
@@ -36,12 +35,43 @@ class _RizerMainPageState extends State<RizerMainPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => setState(() {
-          //TODO: Search for specific player
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SearchPage()));
         }),
         tooltip: 'Search for a specific player.',
         child: Icon(Icons.search),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: RizerConstants.mainTheme,
+      child: new Scaffold(
+        backgroundColor: Colors.white12,
+        body: Center(
+          child: Text("Text"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          shape: const CircularNotchedRectangle(),
+          child: Container(height: 50.0),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          tooltip: '.',
+          child: Icon(Icons.arrow_left),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
     );
   }
 }
